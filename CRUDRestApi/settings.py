3f5339af52749332adbadf90aa452acbb2794707
kamwanasamwel <kamwanasamwel@gmail.com>
2018-05-16 12:31:43 +0300

    after succesful test run

--- CRUDRestApi/settings.py
+++ CRUDRestApi/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'animals',
-    'rest-framework',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'CRUDRestApi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crudrestapi',
+        'USER': 'restapi',
+        'PASSWORD': 'devhack2',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CRUDRestApi
+ a CRUD-based RESTful API with Django and Django REST Framework, which is used for rapidly building RESTful APIs based on Django models.
